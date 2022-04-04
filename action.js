9a596894d36e95dd0691a45881a17cdffc773280
Kwamboka <kwambokaj2.jk@gmail.com>
2022-04-04 19:36:33 +0300

    changes

--- action.js
+++ action.js
@@ -1,12 +1,21 @@
-function toggle(){
-    let div = document.querySelector('.close-page')
-    if(div.style.display==="none"){
-        div.style.display="block"
-    }else{
-        div.style.display="none"
-    }
-    console.log(div)
-}
+let div = document.querySelector('.close-page');
+let open= document.querySelector('.open');
+let close= document.querySelector('.close');
+let links = document.querySelector('li');
+
+open.addEventListener('click',()=>{
+ div.style.display="block";
+})
+
+close.addEventListener('click',()=>{
+ div.style.display="none";
+})
+
+links.addEventListener('click',()=>{
+    div.style.display="none";
+
+   console.log("kerry")
+})
 
 
 
