191b18be64eae705fef2af52b741113a365393d3
Kwamboka <kwambokaj2.jk@gmail.com>
2022-04-04 14:34:41 +0300

    Create event

---
+++ action.js
@@ -0,0 +1,12 @@
+function toggle(){
+    let div = document.querySelector('.close-page')
+    if(div.style.display==="none"){
+        div.style.display="block"
+    }else{
+        div.style.display="none"
+    }
+    console.log(div)
+}
+
+
+
