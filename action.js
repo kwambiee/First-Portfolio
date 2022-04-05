dfaed9abeadcd4cddf79596e527fdb2b9bec5a02
Kwamboka <kwambokaj2.jk@gmail.com>
2022-04-05 08:24:45 +0300

    Add functionality to menu options

--- action.js
+++ action.js
@@ -1,21 +1,36 @@
+window.onload = function () {
+let bars=document.querySelector('.bars')
 let div = document.querySelector('.close-page');
 let open= document.querySelector('.open');
 let close= document.querySelector('.close');
-let links = document.querySelector('li');
+let links = document.querySelectorAll('.menu-link');
 
 open.addEventListener('click',()=>{
  div.style.display="block";
+
 })
+
+function redirect(){
+    let location = window.location;
+    let initial = location.pathname;
+    console.log(location)
+
+}
+
+ links.forEach((link)=> {
+    link.addEventListener('click',function(e){
+        // e.preventDefault();
+div.style.display="none";
+reload();
+    })
+ })
 
 close.addEventListener('click',()=>{
  div.style.display="none";
 })
 
-links.addEventListener('click',()=>{
-    div.style.display="none";
-
-   console.log("kerry")
-})
-
-
-
+document.querySelectorAll(".menu-link").forEach(n => n.addEventListener("click",()=> {
+    menu-list.classList.remove("menu-link");
+    // bars.classList.remove("close-page")
+}))
+}
