2fc9f8907c47b5c86f1baf3a578ee2559f0c2d53
Kwamboka <kwambokaj2.jk@gmail.com>
2022-04-05 13:19:30 +0300

    Fix linter errors

--- action.js
+++ action.js
@@ -1,36 +1,37 @@
-window.onload = function () {
-let bars=document.querySelector('.bars')
-let div = document.querySelector('.close-page');
-let open= document.querySelector('.open');
-let close= document.querySelector('.close');
-let links = document.querySelectorAll('.menu-link');
+window.onload = function meunWindow() {
+  // const bars = document.querySelector('.bars');
+  const div = document.querySelector('.close-page');
+  const open = document.querySelector('.open');
+  const close = document.querySelector('.close');
+  const links = document.querySelectorAll('.menu-link');
 
-open.addEventListener('click',()=>{
- div.style.display="block";
+  open.addEventListener('click', () => {
+    div.style.display = 'block';
+  });
 
-})
+  close.addEventListener('click', () => {
+    div.style.display = 'block';
+  });
 
-function redirect(){
-    let location = window.location;
-    let initial = location.pathname;
-    console.log(location)
+  // function redirect() {
+  // const { location } = window;
+  // const initial = location.pathname;
+  // console.log(location);
+  //  }
 
-}
+  links.forEach((link) => {
+    link.addEventListener('click', () => {
+      // e.preventDefault();
+      div.style.display = 'none';
+    });
+  });
 
- links.forEach((link)=> {
-    link.addEventListener('click',function(e){
-        // e.preventDefault();
-div.style.display="none";
-reload();
-    })
- })
+  close.addEventListener('click', () => {
+    div.style.display = 'none';
+  });
 
-close.addEventListener('click',()=>{
- div.style.display="none";
-})
-
-document.querySelectorAll(".menu-link").forEach(n => n.addEventListener("click",()=> {
-    menu-list.classList.remove("menu-link");
-    // bars.classList.remove("close-page")
-}))
-}
+//   document.querySelectorAll('.menu-link').forEach((n) => n.addEventListener('click', () => {
+//     menu - list.classList.remove('menu-link');
+//     // bars.classList.remove("close-page")
+//   }));
+};
