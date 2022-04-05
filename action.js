b699f3162ec5e77157d848fad46859d2118c7660
Kwamboka <kwambokaj2.jk@gmail.com>
2022-04-05 16:00:15 +0300

    Add blur to menu window

--- action.js
+++ action.js
@@ -1,37 +1,41 @@
 window.onload = function meunWindow() {
-  // const bars = document.querySelector('.bars');
   const div = document.querySelector('.close-page');
   const open = document.querySelector('.open');
   const close = document.querySelector('.close');
   const links = document.querySelectorAll('.menu-link');
+  const main = document.querySelector('main');
+
+  function disableScroll() {
+    // Get the current page scroll position
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+    // if any scroll is attempted, set this to the previous value
+    window.onscroll = function () {
+      window.scrollTo(scrollLeft, scrollTop);
+    };
+  }
+  function enableScroll() {
+    window.onscroll = function () {};
+  }
 
   open.addEventListener('click', () => {
     div.style.display = 'block';
-  });
-
-  close.addEventListener('click', () => {
-    div.style.display = 'block';
-  });
-
-  // function redirect() {
-  // const { location } = window;
-  // const initial = location.pathname;
-  // console.log(location);
-  //  }
-
-  links.forEach((link) => {
-    link.addEventListener('click', () => {
-      // e.preventDefault();
-      div.style.display = 'none';
-    });
+    disableScroll();
+    main.style.filter = 'blur(4px)';
   });
 
   close.addEventListener('click', () => {
     div.style.display = 'none';
+    enableScroll();
+    main.style.filter = 'none';
   });
 
-//   document.querySelectorAll('.menu-link').forEach((n) => n.addEventListener('click', () => {
-//     menu - list.classList.remove('menu-link');
-//     // bars.classList.remove("close-page")
-//   }));
+  links.forEach((link) => {
+    link.addEventListener('click', () => {
+      enableScroll();
+      div.style.display = 'none';
+      main.style.filter = 'none';
+    });
+  });
 };
