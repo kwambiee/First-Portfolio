4cc1cc84c0a5492d0d4780c580249de1e0bd2b1f
kwambiee <kwambokaj2.jk@gmail.com>
2022-09-26 13:40:23 +0300

    Add projects

--- action.js
+++ action.js
@@ -1,59 +1,59 @@
 const projectsArray = [
   {
-    title: 'Awesome Books',
+    title: "Awesome Books",
     description:
-      'This is a basic website that allows users to add/remove books from a list. This project is built using Vanilla JavaScript, ES6 and Webpack.',
+      "This is a basic website that allows users to add/remove books from a list. This project is built using Vanilla JavaScript, ES6 and Webpack.",
     modaldesc:
-      "The Awesome Books Website has three Navigation bars. The 'Add new' section allows you enter your favorite books. The 'List' section displays your favorite books and allows you also do remove the books. The contact section displays the address and Telephone contact incase you want to reach me.",
-    imageLink: 'images/Books-shot.png',
-    tags: ['HTML', 'CSS', 'Javascript'],
-    role: 'Front End Dev',
-    company: 'Microverse',
-    year: '2022',
-    sourceLink: 'https://kwambiee.github.io/ES6-AwesomeBooks/',
-    demoLink: 'https://github.com/kwambiee/ES6-AwesomeBooks',
+      "The App has the 'Add new' section that allows you enter your favorite books,the 'List' section that displays your favorite books and allows you to remove books and the contact section that displays address and telephone contact.",
+    imageLink: "images/awesome_books.png",
+    tags: ["HTML", "CSS", "Javascript"],
+    role: "Front End Dev",
+    company: "MICROVERSE",
+    year: "2022",
+    sourceLink: "https://github.com/kwambiee/ES6-AwesomeBooks",
+    demoLink: "https://kwambiee.github.io/ES6-AwesomeBooks/",
   },
   {
-    title: 'Multi-Post Stories',
+    title: "Beauty Parlour",
     description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      "This app extracts data Makeup API and displays a list of different brands, products contained in the brand and product details.",
     modaldesc:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
-    imageLink: 'images/Snap2.png',
-    tags: ['HTML', 'CSS', 'Javascript'],
-    role: 'Back End Dev',
-    company: 'CANOPY',
-    year: '2015',
-    sourceLink: '',
-    demoLink: '',
+      "This application displays all makeup brands, products contained in the brand and product details. It also allows you to view the price and ingredients of the product.",
+    imageLink: "images/makeup.png",
+    tags: ["React", "CSS", "Javascript"],
+    role: "Front End Dev",
+    company: "MICROVERSE",
+    year: "2022",
+    sourceLink: "https://github.com/kwambiee/beauty-parlour",
+    demoLink: "https://www.loom.com/share/2c20791a4710438d92b4ba6e8e28711b",
   },
   {
-    title: 'Tonic',
+    title: "Tonic",
     description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
     modaldesc:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum han printer took a galley of type and scrambled it 1960s',
-    imageLink: 'images/Snap3.png',
-    tags: ['HTML', 'CSS', 'Javascript'],
-    role: 'Back End Dev',
-    company: 'CANOPY',
-    year: '2015',
-    sourceLink: '',
-    demoLink: '',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
+    imageLink: "images/Snap3.png",
+    tags: ["HTML", "CSS", "Javascript"],
+    role: "Back End Dev",
+    company: "CANOPY",
+    year: "2015",
+    sourceLink: "",
+    demoLink: "",
   },
   {
-    title: 'Multi-Post Stories',
+    title: "Budget App",
     description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      "A Ruby project on building a budget application to track your expenditure.",
     modaldesc:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
-    imageLink: 'images/Snapshoot.png',
-    tags: ['HTML', 'CSS', 'Javascript'],
-    role: 'Back End Dev',
-    company: 'CANOPY',
-    year: '2015',
-    sourceLink: '',
-    demoLink: '',
+      "This is a Ruby on Rails capstone project is about building a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what.",
+    imageLink: "images/desk-budget.png",
+    tags: ["Ruby", "Rails", "CSS"],
+    role: "Back End Dev",
+    company: "MICROVERSE",
+    year: "2022",
+    sourceLink: "https://github.com/kwambiee/budget-app",
+    demoLink: "https://kwambiee-budget.herokuapp.com/",
   },
 ];
 
@@ -143,15 +143,15 @@ window.onload = function loader() {
                 ${tags}
               </div>
               <div class="modal-links">
-                <a href="${project.sourceLink}"><span>See live</span> 
+                <a href="${project.demoLink}"><span>See live</span>
                 <img src="./images/live.png"/></a>
-                <a href="${project.demoLink}"><span>See Source</span>
+                <a href="${project.sourceLink}"><span>See Source</span>
                 <img src="./images/giticon.png"/>
                 </a>
               </div>
             </div>
           </div>
-        </div>        
+        </div>
       </div>`;
     });
     modals.innerHTML = modalitem;
@@ -167,7 +167,7 @@ window.onload = function loader() {
       proj += `<div class="desk-cards">
       <div class="desk-object">
         <img class="card-image"src="${project.imageLink}" />
-        <div class="desk-conta"
+        <div class="desk-conta">
           <p class="card-tempm">${project.title}</p>
           <div class="item-details">
             <span>${project.company}</span>
